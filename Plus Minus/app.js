6799166ccdc9358d799b7bd06ba88a36256091d2
Yajaira Gracia <yajairagracia@Yajairas-MBP.attlocal.net>
2022-09-16 13:19:41 -0500

    practices

---
+++ Plus Minus/app.js
@@ -0,0 +1,45 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'plusMinus' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function plusMinus(arr) {
+    let counts = [0, 0, 0]
+    arr.forEach (num => {
+        if (num > 0) counts[0]+=1
+        else if (num < 0) counts[1] +=1
+        else counts[2]+=1
+    })
+     counts.forEach(num => console.log((num/arr.length).toFixed(6)))
+}
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    plusMinus(arr);
+}
